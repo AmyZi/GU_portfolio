524481cb8275a33aead55ca2590859cdbdf0a078
Amyzi <ebiojoidris@icloud.com>
2024-11-27 02:19:36 +0100

    k

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import AcademicDashboard from './pages/StudyPage';
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<WelcomePage />} />
@@ -17,7 +17,7 @@ const App = () => {
         <Route path="/study" element={<AcademicDashboard />} />
       </Routes>
       <Footer />
-    </Router>
+    </HashRouter>
   );
 };
 export default App;
